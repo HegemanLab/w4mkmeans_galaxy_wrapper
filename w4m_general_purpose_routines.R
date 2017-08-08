20bb07b1aa2f3ac7e92e1f60b412a8f9bba8914f
eschen42 <eschen@alumni.princeton.edu>
2017-08-07 22:16:47 -0500

    wrapper seems to work now

---
+++ w4m_general_purpose_routines.R
@@ -0,0 +1,283 @@
+# prepare.data.matrix - Prepare x.datamatrix for multivariate statistical analaysis (MVA)
+#   - Motivation:
+#     - Selection:
+#       - You may want to exclude several samples from your analysis:
+#         - If so, set the argument 'exclude.samples' to a vector of sample names
+#       - You may want to exclude several features or features from your analysis:
+#         - If so, set the argument 'exclude.features' to a vector of feature names
+#     - Renaming samples:
+#       - You may want to rename several samples from your analysis:
+#         - If so, set the argument 'sample.rename.function' to a function accepting a vector 
+#           of sample names and producing a vector of strings of equivalent length
+#     - MVA is confounded by missing values.
+#       - By default, this function imputes missing values as zero.
+#       - For a different imputation, set the 'data.imputation' argument to a function
+#         accepting a single matrix argument and returning  a matrix of the same
+#         dimensions as the argument.
+#     - Transformation
+#       - It may be desirable to transform the intensity data to reduce the range.
+#       - By default, this function performs an eigth-root transformation:
+#         - Any root-tranformation has the advantage of never being negative.
+#         - Calculation of the eight-root is four times faster in my hands than log10.
+#         - However, it has the disadvantage that calculation of fold-differences 
+#           is not additive as with log-transformation.
+#           - Rather, you must divide the values and raise to the eighth power.
+#       - For a different transformation, set the 'data.transformation' argument
+#           to a function accepting a single matrix argument.
+#         - The function should be written to return a matrix of the same dimensions
+#           as the argument.
+# arguments:
+#   - x.matrix - matrix of intensities (or data.frame of sample metadata)
+#     - one row per sample
+#     - one column per feature or metadata attribute
+#   - exclude.samples - vector of labels of matrix rows (samples) to omit from analysis
+#   - exclude.features - vector of labels of matrix columnss (features) to omit from analysis
+#   - sample.rename.function - function to be used to rename rows if necessary, or NULL
+#     - e.g., sample.rename.function = function(x) {
+#               sub("(.*)_.*","\\1", row.names(x))
+#             }
+#   - data.imputation - function applied to matrix to impute missing values
+#     - e.g., data.imputation = function(m) {
+#               m[is.na(m)] <- min(m, na.rm = TRUE) / 100
+#               return (m)
+#             }
+#   - data.transformation - function applied to matrix cells
+#     - e.g., data.transformation = function(x) { return( log10(x) ) }
+#         or, data.transformation = log10
+# result value:
+#   transformed, imputed x.datamatrix with renamed rows and with neither excluded values nor features
+#
+################################
+##
+##  Notes regarding the effectiveness and performance of the data transformation method.
+##
+##  The two transformations that I tried (log10 and 8th root) required different imputation methods.
+##
+##  For the LCMS resin data set that I was working with, separation in MVA was nearly equivalent for:
+##    data.imputation <- function(x.matrix) {
+##      x.matrix[is.na(x.matrix)] <- 0
+##      return (x.matrix)
+##    }
+##    data.transformation <- function(x) {
+##      sqrt( sqrt( sqrt(x) ) )
+##    }
+##  and
+##    data.imputation <- function(x.matrix) {
+##      x.matrix[is.na(x.matrix)] <- min(x.matrix, na.rm = TRUE) / 100
+##      return (x.matrix)
+##    }
+##    data.transformation <- function(x) {
+##      log10(x)
+##    }
+##
+##  Note further that triple application of the square root:
+##  - may be four times faster than log10:
+##  - may be three times faster than log2:
+##
+##      system.time( junk <- sqrt( sqrt( sqrt(1:100000000) ) ) )
+##         user  system elapsed
+##        0.832   0.236   1.069
+##      system.time( junk <- log10(1:100000000) )
+##         user  system elapsed
+##        3.936   0.400   4.337
+##      system.time( junk <- log2(1:100000000) )
+##         user  system elapsed
+##        2.784   0.320   3.101
+##
+################################
+#
+prepare.data.matrix <- function(
+  x.matrix
+, exclude.samples = NULL
+, exclude.features = NULL
+, sample.rename.function = NULL
+, data.imputation =
+    function(m) {
+      # replace NA values with zero
+      m[is.na(m)] <- 0
+      # replace negative values with zero, if applicable (It should never be applicable!)
+      if (min(m < 0)) {
+        m <- matrix(lapply(X = m, FUN = function(z) {max(z,0)}), nrow = nrow(m) )
+      }
+      # return matrix as the result
+      return (m)
+    }
+, data.transformation = function(x) {
+    sqrt( sqrt( sqrt(x) ) )
+  }
+, en = new.env()
+) {
+  # MatVar - Compute variance of rows or columns of a matrix
+  # ref: http://stackoverflow.com/a/25100036
+  # For row variance, dim == 1, for col variance, dim == 2
+  MatVar <- function(x, dim = 1) {
+    if (dim == 1) {
+      dim.x.2 <- dim(x)[2]
+      if ( dim.x.2 == 0 )
+        stop("MatVar: there are zero columns")
+      if ( dim.x.2 == 1 ) {
+        stop("MatVar: a single column is insufficient to calculate a variance")
+        # return ( rep.int(x = 0, times = nrow(x)) )
+      } else {
+        return ( rowSums( (x    - rowMeans(x))^2 ) / ( dim(x)[2] - 1 ) )
+      }
+    } else if (dim == 2) {
+      dim.x.1 <- dim(x)[1]
+      if ( dim.x.1 == 0 ) {
+        stop("MatVar: there are zero rows")
+      }
+      if ( dim.x.1 == 1 ) {
+        stop("MatVar: a single row is insufficient to calculate a variance")
+        # return ( rep.int(x = 0, times = ncol(x)) )
+      } else {
+        return ( rowSums( (t(x) - colMeans(x))^2 ) / ( dim(x)[1] - 1 ) )
+      }
+    } else stop("Please enter valid dimension, for rows, dim = 1; for colums, dim = 2")
+  }
+
+  nonzero.var <- function(x) {
+    if (nrow(x) == 0) {
+      print(str(x))
+      stop("matrix has no rows")
+    }
+    if (ncol(x) == 0) {
+      print(str(x))
+      stop("matrix has no columns")
+    }
+    if ( is.numeric(x) ) {
+      # exclude any rows with zero variance
+      row.vars <- MatVar(x, dim = 1)
+      nonzero.row.vars <- row.vars > 0
+      nonzero.rows <- row.vars[nonzero.row.vars]
+      if ( length(rownames(x)) != length(rownames(nonzero.rows)) ) {
+        row.names <- attr(nonzero.rows,"names")
+        x <- x[ row.names, , drop = FALSE ]
+      }
+      
+      # exclude any columns with zero variance
+      column.vars <- MatVar(x, dim = 2)
+      nonzero.column.vars <- column.vars > 0
+      nonzero.columns <- column.vars[nonzero.column.vars]
+      if ( length(colnames(x)) != length(colnames(nonzero.columns)) ) {
+        column.names <- attr(nonzero.columns,"names")
+        x <- x[ , column.names, drop = FALSE ]
+      }
+    }
+    return (x)
+  }
+
+  if (is.null(x.matrix)) {
+    stop("FATAL ERROR - prepare.data.matrix was called with null x.matrix")
+  }
+
+  en$xpre <- x <- x.matrix
+
+  # exclude any samples as indicated
+  if ( !is.null(exclude.features) ) {
+    my.colnames <- colnames(x)
+    my.col.diff <- setdiff(my.colnames, exclude.features)
+    x <- x[ , my.col.diff , drop = FALSE ]
+  }
+
+  # exclude any features as indicated
+  if ( !is.null(exclude.samples) ) {
+    my.rownames <- rownames(x)
+    my.row.diff <- setdiff(my.rownames, exclude.samples)
+    x <- x[ my.row.diff, , drop = FALSE ]
+  }
+
+  # rename rows if desired
+  if ( !is.null(sample.rename.function) ) {
+    renamed <- sample.rename.function(x)
+    rownames(x) <- renamed
+  }
+
+  # save redacted x.datamatrix to environment
+  en$redacted.data.matrix <- x
+
+  # impute values missing from the x.datamatrix
+  if ( !is.null(data.imputation) ) {
+    x <- data.imputation(x)
+  }
+
+  # perform transformation if desired
+  if ( !is.null(data.transformation) ) {
+    x <- data.transformation(x)
+  } else {
+    x <- x
+  }
+
+  # purge rows and columns that have zero variance
+  if ( is.numeric(x) ) {
+    x <- nonzero.var(x)
+  }
+
+  # save imputed, transformed x.datamatrix to environment
+  en$imputed.transformed.data.matrix <- x
+
+  return(x)
+}
+
+
+##-----------------------------------------------
+## helper functions for error detection/reporting
+##-----------------------------------------------
+
+# log-printing to stderr
+log_print <- function(x, ...) { 
+  cat(
+    format(Sys.time(), "%Y-%m-%dT%H:%M:%S%z")
+  , " "
+  , c(x, ...)
+  , "\n"
+  , sep=""
+  , file=stderr()
+  )
+}
+
+# tryCatchFunc produces a list
+#   On success of expr(), tryCatchFunc produces
+#     list(success TRUE, value = expr(), msg = "")
+#   On failure of expr(), tryCatchFunc produces
+#     list(success = FALSE, value = NA, msg = "the error message")
+tryCatchFunc <- function(expr) {
+  # format error for logging
+  format_error <- function(e) {
+    paste(c("Error { message:", e$message, ", call:", e$call, "}"), collapse = " ")
+  }
+  my_expr <- expr
+  retval <- NULL
+  tryCatch(
+    expr = {
+      retval <- ( list( success = TRUE, value = my_expr(), msg = "" ) )
+    }
+  , error = function(e) {
+      retval <<- list( success = FALSE, value = NA, msg = format_error(e) )
+    }
+  )
+  return (retval)
+}
+
+# tryCatchProc produces a list
+#   On success of expr(), tryCatchProc produces
+#     list(success TRUE, msg = "")
+#   On failure of expr(), tryCatchProc produces
+#     list(success = FALSE, msg = "the error message")
+tryCatchProc <- function(expr) {
+  # format error for logging
+  format_error <- function(e) {
+    paste(c("Error { message:", e$message, ", call:", e$call, "}"), collapse = " ")
+  }
+  retval <- NULL
+  tryCatch(
+    expr = {
+      expr()
+      retval <- ( list( success = TRUE, msg = "" ) )
+    }
+  , error = function(e) {
+      retval <<- list( success = FALSE, msg = format_error(e) )
+    }
+  )
+  return (retval)
+}
+
---
+++ w4mkmeans_wrapper.R
@@ -0,0 +1,318 @@
+#!/usr/bin/env Rscript
+
+# references:
+#   what this does:
+#   - [stats::kmeans](https://stat.ethz.ch/R-manual/R-devel/library/stats/html/kmeans.html)
+#   - [stats::p.adjust](https://stat.ethz.ch/R-manual/R-devel/library/stats/html/p.adjust.html)
+#   how this does what it does:
+#   - [parallel::clusterApply](https://stat.ethz.ch/R-manual/R-devel/library/parallel/html/clusterApply.html)
+
+# invocation:
+#   Rscript $__tool_directory__/w4mkmeans_wrapper.R \
+#     data_matrix_path '$dataMatrix_in' \
+#     variable_metadata_path '$variableMetadata_in' \
+#     sample_metadata_path '$sampleMetadata_in' \
+#     kfeatures '$kfeatures' \
+#     ksamples '$ksamples' \
+#     iter_max '$itermax' \
+#     nstart '$nstart' \
+#     algorithm '$algorithm' \
+#     scores '$scores' \
+#     sampleMetadata_out '$sampleMetadata_out' \
+#     variableMetadata_out '$variableMetadata_out' \
+#     slots "\${GALAXY_SLOTS:-1}" \
+# 
+# <inputs>
+#   <param name="dataMatrix_in" label="Data matrix file" type="data" format="tabular" help="variable x sample, decimal: '.', missing: NA, mode: numerical, separator: tab" />
+#   <param name="sampleMetadata_in" label="Sample metadata file" type="data" format="tabular" help="sample x metadata columns, separator: tab" />
+#   <param name="variableMetadata_in" label="Variable metadata file" type="data" format="tabular" help="variable x metadata columns, separator: tab" />
+#   <param name="kfeatures" label="K value(s) for features" type="text" value="0" help="Single or min,max value(s) for K for features (variables), or 0 for none." />
+#   <param name="ksamples" label="K value(s) for samples" type="text" value="0" help="Single or min,max value(s) for K for samples, or 0 for none." />
+#   <param name="iter_max" label="Max number of iterations" type="text" value="10" help="The maximum number of iterations allowed; default 10." />
+#   <param name="nstart" label="Number of random sets" type="text" value="1" help="How many random sets should be chosen; default 1." />
+# 	<param name="algorithm" label="Algorithm for clustering" type="select" value = "Hartigan-Wong" help="K-means clustering algorithm, default 'Hartigan-Wong'; alternatives 'Lloyd', 'MacQueen'; 'Forgy' is a synonym for 'Lloyd', see stats::kmeans reference for further info and references.">
+# 	  <option value="Hartigan-Wong" selected="TRUE">Hartigan-Wong</option>
+# 	  <option value="Lloyd">Lloyd</option>
+# 	  <option value="MacQueen">MacQueen</option>
+# 	  <option value="Forgy">Forgy</option>
+# 	</param>
+# </inputs>
+# <outputs>
+#   <data name="sampleMetadata_out" label="${tool.name}_${sampleMetadata_in.name}" format="tabular" ></data>
+#   <data name="variableMetadata_out" label="${tool.name}_${variableMetadata_in.name}" format="tabular" ></data>
+# </outputs>
+
+##---------------
+## Initialization
+##---------------
+
+##----------
+## libraries
+##----------
+
+library(batch) ## for 'parseCommandArgs'
+
+##---------------------------------------------------
+## Computation - general and module-specific routines
+##---------------------------------------------------
+
+source("w4m_general_purpose_routines.R")
+source("w4mkmeans_routines.R")
+
+##-----------------------------------------
+## Computation - W4m data-suppport routines
+##-----------------------------------------
+
+# read_data_frame - read a w4m data frame from a tsv, with error handling
+#   e.g., data_matrix_input_env <- read_data_frame(dataMatrix_in, "data matrix input")
+read_data_frame <- function(file_path, kind_string, failure_action = log_print) {
+  my.env <- new.env()
+  my.env$success <- FALSE
+  my.env$msg <- sprintf("no message reading %s", kind_string)
+  tryCatch(
+    expr = {
+      my.env$data    <- utils::read.delim( fill = FALSE, file = file_path )
+      my.env$success <- TRUE
+    }
+  , error = function(e) {
+     my.env$msg <<- sprintf("%s read failed", kind_string)
+    }
+  )
+  if (!my.env$success) {
+    failure_action(my.env$msg)
+  }
+  return (my.env)
+}
+
+# write_result - write a w4m data frame to a tsv
+write_result <- function(result, file_path, kind_string, failure_action = log_print) {
+  my.env <- new.env()
+  my.env$success <- FALSE
+  my.env$msg <- sprintf("no message writing %s", kind_string)
+  tryCatch(
+    expr = {
+      write.table(
+        x = result
+      , sep = "\t"
+      , file = file_path
+      , quote = FALSE
+      , row.names = FALSE
+      )
+      my.env$success <- TRUE
+    }
+  , error = function(e) {
+     my.env$msg <<- sprintf("%s write failed", kind_string)
+    }
+  )
+  if (!my.env$success) {
+    failure_action(my.env$msg)
+    return (my.env)
+  }
+  return (my.env)
+}
+
+# read the three input files
+read_input_data <- function(env, failure_action = log_print) {
+  kind_string <- "none"
+  tryCatch(
+    expr = {
+      # read in the sample metadata
+      kind_string <- "sample metadata input"
+      smpl_metadata_input_env <- 
+        read_data_frame(
+                         file_path = env$sample_metadata_path
+                       , kind_string = kind_string
+                       , failure_action = failure_action
+                       )
+      if (!smpl_metadata_input_env$success) {
+        failure_action(smpl_metadata_input_env$msg)
+        return ( FALSE )
+      }
+      env$sampleMetadata <- smpl_metadata_input_env$data
+
+      # read in the variable metadata
+      kind_string <- "variable metadata input"
+      vrbl_metadata_input_env <- 
+        read_data_frame(
+                         file_path = env$variable_metadata_path
+                       , kind_string = kind_string
+                       , failure_action = failure_action
+                       )
+      if (!vrbl_metadata_input_env$success) {
+        failure_action(vrbl_metadata_input_env$msg)
+        return ( FALSE )
+      }
+      env$variableMetadata <- vrbl_metadata_input_env$data
+
+      # read in the data matrix
+      kind_string <- "data matrix input"
+      data_matrix_input_env <-
+        read_data_frame(
+                         file_path = env$data_matrix_path
+                       , kind_string = kind_string
+                       , failure_action = failure_action
+                       )
+      if (!data_matrix_input_env$success) {
+        failure_action(data_matrix_input_env$msg)
+        return ( FALSE )
+      }
+      # data frame for dataMatrix has rownames in first column
+      data_matrix_df <- data_matrix_input_env$data
+      rownames(data_matrix_df) <- data_matrix_df[,1]
+      data_matrix <- data_matrix_df[,2:ncol(data_matrix_df)]
+      env$dataMatrix <- as.matrix(data_matrix)
+
+    }
+  , error = function(e) {
+     failure_action( sprintf("read_input_data failed for '%s' - %s", kind_string, format_error(e)) )
+     return ( FALSE )
+    }
+  )
+  return ( TRUE )
+}
+
+
+read_input_failure_action <- function(x, ...) { 
+  log_print("Failure reading input for '", modNamC, "' Galaxy module call")
+  log_print(x, ...)
+}
+
+##--------------------------
+## Computation - Entry Point
+##--------------------------
+
+##----------
+## Constants
+##----------
+
+modNamC <- "w4mkmeans" ## module name
+
+argVc <- unlist(parseCommandArgs(evaluate=FALSE))
+
+## options
+##--------
+
+# Set the handler for R error-handling
+options( show.error.messages = F
+       , error = function () { 
+                   log_print( "Fatal error in '", modNamC, "': ", geterrmessage() )
+                   q( "no", 1, F )
+                 }
+       , warn = -1
+       )
+
+# strings as factors? - not by default!
+# save old value
+strAsFacL <- options()$stringsAsFactors
+options(stringsAsFactors = FALSE)
+
+
+## log file
+##---------
+
+log_print("Start of the '", modNamC, "' Galaxy module call")
+
+## arguments
+##----------
+
+args_env <- new.env()
+
+# files
+
+log_print("PARAMETERS (raw):")
+invisible(
+  lapply(
+    X = 1:length(argVc)
+  , FUN = function(i) {
+      log_print(sprintf("  - %s: %s", names(argVc)[i], argVc[i]))
+    }
+  )
+)
+
+# write.table(as.matrix(argVc), col.names=F, quote=F, sep='\t')
+
+## output files
+sampleMetadata_out              <- as.character(argVc["sampleMetadata_out"])
+variableMetadata_out            <- as.character(argVc["variableMetadata_out"])
+scores_out                      <- as.character(argVc["scores_out"])
+## input files
+args_env$data_matrix_path       <- as.character(argVc["data_matrix_path"])
+args_env$variable_metadata_path <- as.character(argVc["variable_metadata_path"])
+args_env$sample_metadata_path   <- as.character(argVc["sample_metadata_path"])
+  
+# other parameters
+
+# multi-string args - split csv: "1,2,3" -> c("1","2","3")
+args_env$kfeatures <- strsplit(x = as.character(argVc['kfeatures']), split = ",", fixed = TRUE)[[1]]
+args_env$ksamples  <- strsplit(x = as.character(argVc['ksamples' ]), split = ",", fixed = TRUE)[[1]]
+# numeric args
+args_env$iter_max  <- as.numeric(               argVc['itermax'  ])
+args_env$nstart    <- as.numeric(               argVc['nstart'   ])
+args_env$slots     <- as.numeric(               argVc['slots'    ])
+# string args
+args_env$algorithm <- as.character(             argVc['algorithm'])
+args_env$log_print <- log_print
+
+log_print("PARAMETERS (parsed):")
+for (member in ls(args_env)) {
+  value <- get(member, args_env)
+  value <- ifelse(length(value) == 1, value, sprintf("c(%s)", paste(value, collapse=", ")))
+  
+  log_print(sprintf("  - %s: %s", member, ifelse( !is.function(value) , value, "function" )))
+}
+log_print("")
+
+##---------------------------------------------------------
+## Computation - attempt to read input data
+##---------------------------------------------------------
+if ( ! read_input_data(args_env, failure_action = read_input_failure_action) ) {
+  result <- -1
+} else {
+  log_print("Input data was read successfully.")
+  result <- w4mkmeans(env = args_env)
+  log_print("returned from call to w4mkmeans.")
+}
+
+if ( length(result) == 0 ) {
+  log_print("no results were produced")
+  # exit with status code non-zero to indicate error
+  q(save = "no", status = 1, runLast = FALSE)
+} else if ( ! setequal(names(result),c("variableMetadata","sampleMetadata","scores")) ) {
+  log_print(sprintf("unexpected result keys %s", names(result)))
+  # exit with status code non-zero to indicate error
+  q(save = "no", status = 1, runLast = FALSE)
+} else if ( ! write_result(result = result$scores, file_path = scores_out, kind_string = "cluster scores")$success ) {
+  log_print("failed to write output file for cluster scores")
+  # exit with status code non-zero to indicate error
+  q(save = "no", status = 1, runLast = FALSE)
+} else if ( ! write_result(result = result$variableMetadata, file_path = variableMetadata_out, kind_string = "clustered variableMetadata")$success ) {
+  log_print("failed to write output file for clustered variableMetadata")
+  # exit with status code non-zero to indicate error
+  q(save = "no", status = 1, runLast = FALSE)
+} else if ( ! write_result(result = result$sampleMetadata, file_path = sampleMetadata_out, kind_string = "clustered sampleMetadata")$success ) {
+  log_print("failed to write output file for clustered sampleMetadata")
+  # exit with status code non-zero to indicate error
+  q(save = "no", status = 1, runLast = FALSE)
+}
+
+##--------
+## Closing
+##--------
+
+
+if (!file.exists(sampleMetadata_out)) {
+  log_print(sprintf("ERROR %s::w4m_kmeans_wrapper - file '%s' was not created", modNamC, sampleMetadata_out))
+}
+
+if (!file.exists(variableMetadata_out)) {
+  log_print(sprintf("ERROR %s::w4m_kmeans_wrapper - file '%s' was not created", modNamC, variableMetadata_out))
+}
+
+if (!file.exists(scores_out)) {
+  log_print(sprintf("ERROR %s::w4m_kmeans_wrapper - file '%s' was not created", modNamC, scores_out))
+}
+
+log_print("Normal termination of '", modNamC, "' Galaxy module call")
+
+# exit with status code zero
+q(save = "no", status = 0, runLast = FALSE)
